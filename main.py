bddc350bce8c943723fc49e6bd43be81ad50f303
meghna <meghnamandawra@gmail.com>
2024-05-09 20:03:51 +0530

    add sky conditions

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import streamlit as st
+import plotly.express as px
+from backend import get_data
 
 st.title("Weather Forecast for the Next Days")
 place = st.text_input("Place:  ")
@@ -10,3 +12,21 @@ option = st.selectbox("Select date to view",
                       ("Temprature","Sky"))
 
 st.subheader(f"{option} for the next {days} days in {place}")
+
+if place:
+  try:
+    filtered_data = get_data(place, days)
+    if option =="Temprature":
+        tempratures = [dict["main"]["temp"]/10 for dict in filtered_data]
+        dates = [dict["dt_txt"] for dict in filtered_data]
+        figure = px.line(x=dates, y=tempratures, labels={"x":"Dates" , "y":"Temprature (C)"})
+        st.plotly_chart(figure)
+
+    if option == "Sky":
+        images = {"Clear": "images/clear.png", "Clouds":"images/cloud.png",
+                  "Rain": "images/rain.png" , "Snow": "images/snow.png"}
+        sky_conditions = [dict["weather"][0]["main"] for dict in filtered_data]
+        image_paths = [images[condition] for condition in sky_conditions]
+        st.image(image_paths, width = 115)
+  except keyError:
+      print("this place doesn't exists")
